1a7c4d177fe35fffc57a46d46bfddf5bcab44fcf
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-11 08:50:18 -0400

    Added destroy, edit, update, new and create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,19 +7,36 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
-
+  
   def new
-    
+    @task = Task.new
   end
   
   def create
-    
+    Task.create!(params_tasks)
+    redirect_to tasks_path
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params_tasks)  
+    redirect_to tasks_path
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
   
   private
 
   def params_tasks
-
+     params.require(:task).permit(:title, :details)
   end
 
 end
